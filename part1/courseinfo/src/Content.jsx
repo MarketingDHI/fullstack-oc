e7e5200cda9ff2fa3a5431473975b2a6cde5082d
Christine Brovkina <christine.brovkina@digitalhubindustry.de>
2025-12-09 08:46:14 +0100

    1.2: Course Information, step 2 ready

--- part1/courseinfo/src/Content.jsx
+++ part1/courseinfo/src/Content.jsx
@@ -1,8 +1,11 @@
+import Part from "./Part"
+
 const Content = (props) => {
     return (
         <>
-            <p> {props.part} {props.exercises}
-            </p>
+            <Part part={props.part1} exercises={props.exercises1} />
+            <Part part={props.part2} exercises={props.exercises2} />
+            <Part part={props.part3} exercises={props.exercises3} />
         </>
     )
 }
